597b2477e16d9c3ea714b7c7c9238a72d3f18526
Ganapati <ganapatasakatwa@gmail.com>
2023-05-27 19:34:36 +0530

    [GK] Creating Contact

---
+++ AddressBookProblem/AddressBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class AddressBook
+    {
+        List<Contact> con= new List<Contact>();
+        public void AddContact()
+        {
+            
+            Contact contact= new Contact();
+            Console.WriteLine("Enter Fisrt Name");
+            contact.Firstname = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            contact.Lastname = Console.ReadLine();
+            Console.WriteLine("Enter Address Name");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City Name");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State Name");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter e mail");
+            contact.email = Console.ReadLine();
+
+        }
+       
+    }
+}
---
+++ AddressBookProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AddressBookProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            while(true)
+            {
+                Console.WriteLine("Enter The Choice \n1)Create Contact");
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        AddressBook addressBook = new AddressBook();
+                        addressBook.AddContact();
+                        break;
+                    default:
+                        Console.WriteLine("Select Proper Choice");
+                        break;
+                }
+               
+            }
+           
+        }
+    }
+}
