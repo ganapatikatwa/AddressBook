3617774970f2fb5a66f514238acc4786e89af835
Ganapati <ganapatasakatwa@gmail.com>
2023-05-28 10:56:10 +0530

    [GK] Adding the New Contact to the List

--- AddressBookProblem/AddressBook.cs
+++ AddressBookProblem/AddressBook.cs
@@ -8,11 +8,12 @@ namespace AddressBookProblem
 {
     public class AddressBook
     {
-        List<Contact> con= new List<Contact>();
-        public void AddContact()
+        List<Contact> addressbook = new List<Contact>();
+       
+        public void CreateContact()
         {
-            
-            Contact contact= new Contact();
+
+            Contact contact = new Contact();
             Console.WriteLine("Enter Fisrt Name");
             contact.Firstname = Console.ReadLine();
             Console.WriteLine("Enter Last Name");
@@ -29,7 +30,54 @@ namespace AddressBookProblem
             contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Enter e mail");
             contact.email = Console.ReadLine();
+            addressbook.Add(contact);
 
+        }
+        public void AddContact()
+        {
+            Contact newcontact=new Contact();
+            Console.WriteLine("Enter the FirstName");
+            newcontact.Firstname = Console.ReadLine();
+            foreach(Contact contact in addressbook)
+            {
+                if(contact.Firstname==newcontact.Firstname)
+                {
+                    Console.WriteLine("Person with this Name Already Exists");
+                    return;
+                }
+            }
+           
+            Console.WriteLine("Enter Last Name");
+            newcontact.Lastname = Console.ReadLine();
+            Console.WriteLine("Enter Address Name");
+            newcontact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City Name");
+            newcontact.City = Console.ReadLine();
+            Console.WriteLine("Enter State Name");
+            newcontact.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code");
+            newcontact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            newcontact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter e mail");
+            newcontact.email = Console.ReadLine();
+            addressbook.Add(newcontact);
+
+        }
+        public void Display()
+        {
+            foreach(Contact contact in addressbook)
+            {
+                Console.WriteLine("FirstName--"+contact.Firstname);
+                Console.WriteLine("LastName--" + contact.Lastname);
+                Console.WriteLine("Address--" + contact.Address);
+                Console.WriteLine("City--" + contact.City);
+                Console.WriteLine("State--" + contact.State);
+                Console.WriteLine("Zip--" + contact.Zip);
+                Console.WriteLine("PhoneNumber--" + contact.PhoneNumber);
+                Console.WriteLine("Email--" + contact.email);
+
+            }
         }
        
     }
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -8,14 +8,20 @@ namespace AddressBookProblem
             Console.WriteLine("Welcome to Address Book Program");
             while(true)
             {
-                Console.WriteLine("Enter The Choice \n1)Create Contact");
+                AddressBook adBook = new AddressBook();
+                Console.WriteLine("Enter The Choice \n1)Create Contact\n2)Add Contact");
                 int choice=Convert.ToInt32(Console.ReadLine());
                 switch(choice)
                 {
                     case 1:
-                        AddressBook addressBook = new AddressBook();
-                        addressBook.AddContact();
+                        adBook.CreateContact();
+                        adBook.Display();
                         break;
+                    case 2:
+                        adBook.AddContact();
+                        adBook.Display();
+                        break;
+
                     default:
                         Console.WriteLine("Select Proper Choice");
                         break;
